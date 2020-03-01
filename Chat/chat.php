1481bf7e866d52a14183e0cd00742ccced8822e0
jayesh-w <58288373+jayesh-w@users.noreply.github.com>
2020-03-01 10:39:29 +0530

    Update chat.php

--- Chat/chat.php
+++ Chat/chat.php
@@ -79,8 +79,8 @@ function post()
   return false;
 }
 function speak(){
-			var comment1 = document.getElementById("result");
-            responsiveVoice.speak("message "+comment1.value, "Hindi Male",{rate: 0.9});    
+			var comment1 =document.getElementByTag("strong").value;
+            responsiveVoice.speak("message "+comment1, "Hindi Male",{rate: 0.9});    
         }
 </script>
 <script>
